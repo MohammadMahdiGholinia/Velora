f9a95bec9a3ab7ba36ff9ecc534631bcd234b2af
Mohammad Mahdi Gholinia <mohammadmahdigholinia220@gmail.com>
2026-06-18 19:48:18 +0330

    add admin & tour model

--- tours/admin.py
+++ tours/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
-
+from .models import Location , Tour
 # Register your models here.
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name' , )
+    search_fields = ('name' , )
+
+@admin.register(Tour)
+class TourAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'origin',
+        'destination',
+        'price',
+        'capacity',
+        'is_featured',
+        'is_active',
+    )
+
+    list_filter = (
+        'is_featured',
+        'is_active',
+        'origin',
+        'destination',
+    )
+
+    search_fields = (
+        'title',
+        'description',
+        'hotel',
+        'slug',
+    )
+
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
+
+    ordering = ('-id',)
--- tours/models.py
+++ tours/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
+
+class Tour(models.Model):
+    TOUR_TYPE=[
+        ('internal' , 'Internal') ,
+        ('external' , 'external')
+    ]
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    descripton = models.TextField()
+    origin = models.ForeignKey(Location, on_delete=models.CASCADE , related_name='origin')
+    destination = models.ForeignKey(Location, on_delete=models.CASCADE , related_name='destination')
+    cover = models.ImageField(upload_to = 'tours/cover')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    duration = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+    capacity = models.PositiveIntegerField()
+    hotel = models.CharField(max_length=50)
+    is_featured = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+
+
+
