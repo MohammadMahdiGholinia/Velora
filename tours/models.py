f0452030d7099d6ec2b5309690057aa74171d657
Mohammad Mahdi Gholinia <mohammadmahdigholinia220@gmail.com>
2026-06-29 00:19:41 +0330

    edit cover & images model

--- tours/models.py
+++ tours/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django_jalali.db import models as jmodels
 from django.core.validators import MinValueValidator, MaxValueValidator
+from cloudinary.models import CloudinaryField
+
 
 
 # Create your models here.
@@ -24,7 +26,8 @@ class Tour(models.Model):
     description = models.TextField()
     origin = models.ForeignKey(Location, on_delete=models.CASCADE , related_name='origin')
     destination = models.ForeignKey(Location, on_delete=models.CASCADE , related_name='destination')
-    cover = models.ImageField(upload_to = 'tours/cover')
+    # cover = models.ImageField(upload_to = 'tours/cover')
+    cover = CloudinaryField('cover')
     start_date = jmodels.jDateField()
     end_date = jmodels.jDateField()
     duration = models.PositiveIntegerField()
@@ -42,7 +45,8 @@ class Tour(models.Model):
 
 class TourImage (models.Model):
     tour = models.ForeignKey(Tour, on_delete=models.CASCADE, related_name='images')
-    images = models.ImageField(upload_to='tours/gallery')
+    # images = models.ImageField(upload_to='tours/gallery')
+    images = CloudinaryField('images')
 
     def __str__(self):
         return f'گالری {self.tour.title}'
