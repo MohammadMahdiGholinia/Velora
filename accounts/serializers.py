6053b03ba158a4e7f3c339d0a0be71dfd5e8a9b6
Mohammad Mahdi Gholinia <mohammadmahdigholinia220@gmail.com>
2026-07-05 22:59:33 +0330

    fixed login

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import Profile
+from django.contrib.auth import authenticate
+
 
 class LoginSerializer(serializers.Serializer):
     phone = serializers.CharField()
