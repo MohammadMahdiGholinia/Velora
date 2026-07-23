e3e1b71a9c150e22466370e3c3cfdbe7485642c9
Mohammad Mahdi Gholinia <mohammadmahdigholinia220@gmail.com>
2026-07-24 00:00:13 +0330

    fixed bugs

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from tours.models import Tour
 from .serializers import BookingSerializer
 
